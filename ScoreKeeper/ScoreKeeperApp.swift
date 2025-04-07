ea6c03e1862ba49dc82f714edb0e6c3b0360916e
Advocates Close <javaad.ali@advocatesclose.nyc>
2025-04-07 18:28:31 -0500

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by AdvocatesClose on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
